3c802a70de4fab9a70bcac75f1ad5ded8fee55fd
Shivam583-hue <shivamshivamshivam456@gmail.com>
2024-12-14 20:33:07 +0000

    added create button

---
+++ Buttons/CreateButton.tsx
@@ -0,0 +1,16 @@
+<motion.button
+  whileHover={{ scale: 1.07 }}
+  whileTap={{ scale: 0.95 }}
+  className="flex items-center justify-center px-3 md:px-4 py-1 text-sm md:text-base bg-[#6771b9] hover:bg-[#4d4f87] text-white rounded-xl"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className="w-4 h-4"
+  >
+    <path d="M17.414 2.586a2 2 0 112.828 2.828l-14.83 14.83a1 1 0 01-.414.26l-4 1a1 1 0 01-1.225-1.225l1-4a1 1 0 01.26-.414l14.83-14.83zM16.586 4L4 16.586V19h2.414L20 7.414 16.586 4z" />
+  </svg>
+
+  <span className="pl-2"> Create capsule</span>
+</motion.button>;
