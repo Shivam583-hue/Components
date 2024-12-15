e22617f92f4afecbe884e81a861b10722ee3a6d1
Shivam583-hue <shivamshivamshivam456@gmail.com>
2024-12-15 10:04:50 +0000

    Added share button

---
+++ Buttons/ShareButton.tsx
@@ -0,0 +1,35 @@
+<motion.button
+  whileHover={ { scale: 1.07 } }
+whileTap = {{ scale: 1 }}
+className = " mt-4 ml-5 flex items-center focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 gap-2 px-4 py-2 
+rounded - 2xl bg - gradient - to - r from - gray - 700 to - gray - 900
+text - white font - bold shadow - lg
+hover: from - gray - 600 hover: to - gray - 800 "
+  >
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+width = "14"
+height = "14"
+fill = "none"
+viewBox = "0 0 1200 1227"
+  >
+  <path
+      fill="#fff"
+d = "M714.163 519.284 1160.89 0h-105.86L667.137 450.887 357.328 0H0l468.492 681.821L0 1226.37h105.866l409.625-476.152 327.181 476.152H1200L714.137 519.284h.026ZM569.165 687.828l-47.468-67.894-377.686-540.24h162.604l304.797 435.991 47.468 67.894 396.2 566.721H892.476L569.165 687.854v-.026Z"
+  />
+  </svg>
+  < span className = "pl-3 text-lg font-mono font-semibold flex items-center" >
+    Share
+    < span className = "pl-3" >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+height = "18px"
+viewBox = "0 -960 960 960"
+width = "18px"
+fill = "#e8eaed"
+  >
+  <path d="M240-40q-33 0-56.5-23.5T160-120v-440q0-33 23.5-56.5T240-640h120v80H240v440h480v-440H600v-80h120q33 0 56.5 23.5T800-560v440q0 33-23.5 56.5T720-40H240Zm200-280v-447l-64 64-56-57 160-160 160 160-56 57-64-64v447h-80Z" />
+    </svg>
+    </span>
+    </span>
+    </motion.button>;
